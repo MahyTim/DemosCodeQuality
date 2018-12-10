91b74684f1d2cbd312563431469b565b877da23e
TimM2 <timm@infosupport.com>
2018-12-10 15:49:16 +0100

    starter

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Bogus;
+using Bogus.DataSets;
 
 namespace Starter
 {
@@ -6,7 +8,33 @@ namespace Starter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Algorithm.Process();
+        }
+    }
+
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public override string ToString()
+        {
+            return $"{Firstname} {Lastname}";
+        }
+    }
+
+    class Algorithm
+    {
+        public static void Process()
+        {
+            while (true)
+            {
+                var person = new Person()
+                {
+                    Firstname = Console.ReadLine()
+                };
+                person.Lastname = new Faker<Person>("nl").RuleFor(z => z.Lastname, z => z.Name.LastName(Name.Gender.Male)).Generate().Lastname;
+                Console.WriteLine($"Hello {person}");
+            }
         }
     }
 }
