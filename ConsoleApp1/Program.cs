67d82d97ff8def98164bc40328c68abc17d22ebf
TimM2 <timm@infosupport.com>
2018-12-10 19:35:03 +0100

    decorator

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Features.Metadata;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //var builder = new ContainerBuilder();
+    //builder.RegisterType<SaveCommand>().As<ICommand>().WithMetadata("Name", "Save File");
+    //builder.RegisterType<OpenCommand>().As<ICommand>().WithMetadata("Name", "Open File");
+    //builder.RegisterType<EditorWindow>();
+
+    //builder.RegisterAdapter<Meta<ICommand>, ToolbarButton>(cmd => new ToolbarButton(cmd.Value, (string)cmd.Metadata["Name"]));
+
+    //using (var container = builder.Build())
+    //{
+    //    var window = container.Resolve<EditorWindow>();
+    //}
+
+    //public interface ICommand
+    //{
+    //    void Execute();
+    //}
+
+    //public class OpenCommand : ICommand
+    //{
+    //    public void Execute()
+    //    {
+    //        // Save the current image
+    //    }
+    //}
+    //public class SaveCommand : ICommand
+    //{
+    //    public void Execute()
+    //    {
+    //        // Save the current image
+    //    }
+    //}
+
+    //public class ToolbarButton
+    //{
+    //    ICommand _command;
+    //    private readonly string _description;
+
+    //    public ToolbarButton(ICommand command,string description)
+    //    {
+    //        _command = command;
+    //        _description = description;
+    //    }
+    //    public void Click()
+    //    {
+    //        _command.Execute();
+    //    }
+    //}
+
+    //public class EditorWindow
+    //{
+    //    public EditorWindow(IEnumerable<ToolbarButton> toolbarButtons)
+    //    {
+    //        Console.WriteLine(toolbarButtons.Count());
+    //    }
+    //}
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    var builder = new ContainerBuilder();
+
+
+//    builder.RegisterType<SaveCommand>()
+//    .Named<ICommand>("handler");
+//    builder.RegisterType<OpenCommand>()
+//    .Named<ICommand>("handler");
+
+//    builder.RegisterDecorator<ICommand>(
+//    (c, inner) => new CommandHandlerDecorator(inner),
+//    fromKey: "handler");
+
+//    var container = builder.Build();
+
+//    var handlers = container.Resolve<IEnumerable<ICommand>>();
+//    foreach (var handler in handlers)
+//    {
+//    handler.Execute();
+//    }
+
+//public class CommandHandlerDecorator : ICommand
+//    {
+//        private readonly ICommand _command;
+
+//        public CommandHandlerDecorator(ICommand command)
+//        {
+//            _command = command;
+//        }
+
+//        public void Execute()
+//        {
+//            Console.WriteLine("Decorator start");
+//            _command.Execute();
+//            Console.WriteLine("Decorator end");
+//        }
+//    }
+
+//    public interface ICommand
+//    {
+//        void Execute();
+//    }
+
+//    public class OpenCommand : ICommand
+//    {
+//        public void Execute()
+//        {
+//            Console.WriteLine("Open");
+//        }
+//    }
+//    public class SaveCommand : ICommand
+//    {
+//        public void Execute()
+//        {
+//            Console.WriteLine("Save");
+//        }
+//    }
+}
