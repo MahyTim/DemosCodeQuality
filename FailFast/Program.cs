7d942de4352e7591de02aeab10db31eafa08b1c5
TimM2 <timm@infosupport.com>
2018-12-11 11:57:45 +0100

    fail fast

---
+++ FailFast/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net.Http;
+
+namespace FailFast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ FailingApplication/Controllers/ExternalUsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FailingApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExternalUsersController : ControllerBase
+    {
+        private static Random RandomTiming = new Random(13);
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string email)
+        {
+            if (RandomTiming.NextDouble() > 0.5)
+                return BadRequest("you are unlucky and have timedout");
+            return Ok(email);
+        }
+    }
+}
