217a990458159cd80f481865e832b5813f19934a
TimM2 <timm@infosupport.com>
2018-12-11 15:15:41 +0100

    structs

---
+++ Primitives/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Primitives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+        }
+    }
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public uint Age { get; set; }
+    }
+}
