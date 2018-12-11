1c3d59663714632deca0b31fc36ef6c32f97f5af
TimM2 <timm@infosupport.com>
2018-12-11 17:25:50 +0100

    serilog

---
+++ SeriLogDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Security.Principal;
+using Serilog;
+using Serilog.Context;
+using Serilog.Sinks.ContextRollingFile;
+using Serilog.Sinks.Elasticsearch;
+
+namespace SeriLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate:"{Message}")
+                .CreateLogger();
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, "Something went wrong");
+            }
+            Log.CloseAndFlush();
+        }
+    }
+}
